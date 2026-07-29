88e79a668cca56881d3cecf0e70ed0370e075c09
hannganteng <hannforscholl@gmail.com>
2026-07-29 10:23:02 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>cihuysss</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus placeat commodi dolore temporibus, similique recusandae. Aspernatur architecto quidem facilis numquam magni voluptatem, culpa iste, animi in temporibus consequuntur eos distinctio.</p>
+    <ul>
+        <li>ji</li>
+        <li>ro</li>
+        <li>luu</li>
+    </ul>
 </body>
 </html>
